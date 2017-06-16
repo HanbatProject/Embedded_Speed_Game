d952ae1e8af674869519ebb08996610804a82449
SsangWoo <tkddn204@gmail.com>
2017-06-16 16:22:45 +0900

    modify makefile errors...

--- functions.h
+++ functions.h
@@ -1,14 +1,16 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#include "pthread.h"
+
 // 기계 쓰레드 함수들
-void print_text(pthread_mutex_t print_text_mutex);
-void dot_matrix(pthread_mutex_t dot_matrix_mutex);
-void keypad(pthread_mutex_t keypad_mutex);
+void print_text(pthread_mutex_t);
+void dot_matrix(pthread_mutex_t);
+void keypad(pthread_mutex_t);
 
 // 상태에 따른 변수 변경 함수들
-void state_change(pthread_mutex_t current_state_mutex);
-void text_change(pthread_mutex_t current_state_mutex);
+void state_change(pthread_mutex_t);
+void text_change(pthread_mutex_t);
 void score_change();
 void time_change();
 void next_question();
